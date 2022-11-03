8d9fdada7df14d64313a9dd459c76ec2d485c558
Eder Jose <e.pariona@costamar.com>
2022-11-02 19:17:11 -0500

    initial commit

---
+++ src/main/java/com/ederrafo/soapservice1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ederrafo.soapservice1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SoapService1Application.class);
+    }
+
+}
---
+++ src/main/java/com/ederrafo/soapservice1/SoapService1Application.java
@@ -0,0 +1,13 @@
+package com.ederrafo.soapservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapService1Application.class, args);
+    }
+
+}
